b78ee61ee99c1a017228c4fa1a2cbe4e4dfc0e3e
Matthew Pagan <mastapegs01@gmail.com>
2021-08-31 13:25:16 -0400

    refactor promise and add debug launch file

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ var homeTeamName = document.querySelector("#homeName");
 var awayTeamImage = document.querySelector("#awayLogo");
 var awayTeamName = document.querySelector("#awayName");
 var lastMatchGoal = document.querySelector("#goals");
-var leagueName= document.querySelector("#leagueName");
-var leagueCountry= document.querySelector("#leagueCountry");
-var leagueLogo= document.querySelector("#leagueLogo");
+var leagueName = document.querySelector("#leagueName");
+var leagueCountry = document.querySelector("#leagueCountry");
+var leagueLogo = document.querySelector("#leagueLogo");
 var matchTable = document.querySelector("#matchTable");
 
 
 
 //the functions to create an element
-function addMatchTile(data){
-    //createing the tile div
+function addMatchTile(data) {
+    //creating the tile div
     var matchtile = document.createElement('div');
     matchtile.classList.add("match-tile");
 
@@ -25,7 +25,7 @@ function addMatchTile(data){
     var homeTileTeamName = document.createElement('p');
     homeTileTeamName.innerHTML = data['teams']['home']['name'];
     var homeTileTeamLogo = document.createElement('img');
-    homeTileTeamLogo.src=data['teams']['home']['logo'];
+    homeTileTeamLogo.src = data['teams']['home']['logo'];
     homeTeam.appendChild(homeTileTeamLogo);
     homeTeam.appendChild(homeTileTeamName);
 
@@ -35,7 +35,7 @@ function addMatchTile(data){
     var awayTileTeamName = document.createElement('p');
     awayTileTeamName.innerHTML = data['teams']['away']['name'];
     var awayTileTeamLogo = document.createElement('img');
-    awayTileTeamLogo.src=data['teams']['away']['logo'];
+    awayTileTeamLogo.src = data['teams']['away']['logo'];
     awayTeam.appendChild(awayTileTeamLogo);
     awayTeam.appendChild(awayTileTeamName);
     //creating the league info
@@ -45,7 +45,7 @@ function addMatchTile(data){
     var officialLeagueName = document.createElement('p');
     officialLeagueName.innerHTML = data['league']['name'];
     var officialLeagueLogo = document.createElement('img');
-    officialLeagueLogo.src =data['league']['logo'];
+    officialLeagueLogo.src = data['league']['logo'];
     leagueInfo.appendChild(officialLeagueName);
     leagueInfo.appendChild(officialLeagueLogo);
 
@@ -70,28 +70,29 @@ fetch("https://v3.football.api-sports.io/fixtures?date=2021-08-31", {
         "x-rapidapi-key": "7c8b6208c94aaaaf7861641703244162"
     }
 })
-.then(response => response.json().then(data => {
-    var matchesList = data['response'];
-    var fixture = matchesList[0]['fixture'];
-    var goals = matchesList[0]['goals'];
-    var teams = matchesList[0]['teams'];
-    var league = matchesList[0]['league'];
-    console.log(matchesList.length);
-   //Now let's set our first match
-   leagueName= league['name'];
-   leagueLogo= league['logo'];
-   
-   homeTeamImage.src = teams['home']['logo'];
-   homeTeamName.innerHTML = teams['home']['name'];
-   awayTeamImage.src = teams['away']['logo'];
-   awayTeamName.innerHTML = teams['away']['name'];
-   lastMatchGoal.innerHTML = goals['home']+ " - " + goals['away'];
+    .then(response => response.json())
+    .then(data => {
+        var matchesList = data['response'];
+        var fixture = matchesList[0]['fixture'];
+        var goals = matchesList[0]['goals'];
+        var teams = matchesList[0]['teams'];
+        var league = matchesList[0]['league'];
+        console.log(matchesList.length);
+        //Now let's set our first match
+        leagueName = league['name'];
+        leagueLogo = league['logo'];
 
-   for(var i = 1; i<matchesList.length;i++){
-       addMatchTile(matchesList[i]);
-   }
+        homeTeamImage.src = teams['home']['logo'];
+        homeTeamName.innerHTML = teams['home']['name'];
+        awayTeamImage.src = teams['away']['logo'];
+        awayTeamName.innerHTML = teams['away']['name'];
+        lastMatchGoal.innerHTML = goals['home'] + " - " + goals['away'];
 
-}))
-.catch(err => {
-    console.log(err);
-});
+        for (var i = 1; i < matchesList.length; i++) {
+            addMatchTile(matchesList[i]);
+        }
+
+    })
+    .catch(err => {
+        console.log(err);
+    });
